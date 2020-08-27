0d14d19402073c284d54672a95d2f5f8448ecddb
Lance McCarthy <lance.mccarthy@progress.com>
2020-08-27 14:00:39 -0400

    Update SubmitToMsftStore.ps1

--- .scripts/SubmitToMsftStore.ps1
+++ .scripts/SubmitToMsftStore.ps1
@@ -11,8 +11,11 @@ $tenantId = $env:PartnerCenterTenantId
 $sbTempFolderPath = New-Item -Type Directory -Force -Path (Join-Path -Path 'D:\a\MediaFileManager\MediaFileManager\' -ChildPath 'SBTemp')
 
 # ********* Install StoreBroker and import PowerShell Module *********
-git clone https://github.com/Microsoft/StoreBroker.git 'D:\a\MediaFileManager\MediaFileManager\SBGitRoot\'
-Import-Module -Force 'D:\a\MediaFileManager\MediaFileManager\SBGitRoot\StoreBroker'
+#git clone https://github.com/Microsoft/StoreBroker.git 'D:\a\MediaFileManager\MediaFileManager\SBGitRoot\'
+#Import-Module -Force 'D:\a\MediaFileManager\MediaFileManager\SBGitRoot\StoreBroker'
+
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-Module -Name StoreBroker
 
 # ********* Authenticate Store Broker *********
 $cred = New-Object System.Management.Automation.PSCredential ($username, $password)
@@ -26,4 +29,4 @@ New-SubmissionPackage -ConfigPath $configFilePath -AppxPath $appxUploadFilePath 
 $submissionDataPath = Join-Path -Path $sbTempFolderPath -ChildPath 'submission.json'
 $submissionPackagePath = Join-Path -Path $sbTempFolderPath -ChildPath 'submission.zip'
 
-Update-ApplicationSubmission -ReplacePackages -AppId $appStoreId -SubmissionDataPath $submissionDataPath -PackagePath $submissionPackagePath -AutoCommit -Force -NoStatus
+Update-ApplicationSubmission -Verbose -ReplacePackages -AppId $appStoreId -SubmissionDataPath $submissionDataPath -PackagePath $submissionPackagePath -AutoCommit -Force -NoStatus
