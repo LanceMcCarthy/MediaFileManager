3c8c4f347446192061788e93c9c058f66a3917d4
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-04-12 17:00:38 -0400

    Create separate About and Help windows, update Windows 7 warning suppression list

--- src/MediaFileManager/MediaFileManager.Desktop/Windows/AboutWindow.xaml.cs
+++ src/MediaFileManager/MediaFileManager.Desktop/Windows/AboutWindow.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Windows;
-using MarkdownSharp;
 using Microsoft.AppCenter.Crashes;
 using Telerik.Windows.Controls;
-using Telerik.Windows.Documents.FormatProviders.Html;
+using Windows.ApplicationModel;
 
 namespace MediaFileManager.Desktop.Windows
 {
@@ -17,19 +14,14 @@ namespace MediaFileManager.Desktop.Windows
             Loaded += AboutWindow_Loaded;
         }
 
-        private async void AboutWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Irrelevant")]
+        private void AboutWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             try
             {
-                this.Header = $"Media File Manager (v.{typeof(AboutWindow).Assembly.GetName().Version}) - Help & About";
+                var uwpPackageVersion = $"{Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}";
 
-                using var client = new HttpClient();
-                var markdownText = await client.GetStringAsync(new Uri("https://raw.githubusercontent.com/LanceMcCarthy/MediaFileManager/main/.github/other/help.md")).ConfigureAwait(true);
-
-                var markdown = new Markdown();
-                var convertedHtml = markdown.Transform(markdownText);
-
-                RichTextBox.Document = new HtmlFormatProvider().Import(convertedHtml);
+                Header = $"Media File Manager (v.{uwpPackageVersion}) - About";
             }
             catch (Exception ex)
             {
@@ -40,8 +32,7 @@ namespace MediaFileManager.Desktop.Windows
             }
             finally
             {
-                BusyIndicator.IsBusy = false;
-                BusyIndicator.Visibility = Visibility.Collapsed;
+
             }
         }
     }
