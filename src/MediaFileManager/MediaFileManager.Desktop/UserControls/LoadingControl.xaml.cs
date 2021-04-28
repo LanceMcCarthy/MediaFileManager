c08a0a898c10ab0fda21599f0882fa181569599f
Lance McCarthy <lance@dvlup.com>
2021-04-28 14:46:27 -0400

    Updated splash screen to use transparency

---
+++ src/MediaFileManager/MediaFileManager.Desktop/UserControls/LoadingControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MediaFileManager.Desktop.UserControls
+{
+    public partial class LoadingControl : UserControl
+    {
+        public LoadingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
