797c526cfa2f2728ba7207a2d6d4a243c861e82d
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-11-18 19:55:34 -0500

    Fix build msix script

--- .scripts/BuildMsixPackages.ps1
+++ .scripts/BuildMsixPackages.ps1
@@ -1,16 +1,28 @@
+Write-Output "Setting msbuild Path"
 $env:Path += ";C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin"
 
-Set-Location ..\MediaFileManager
+Write-Output "Determining paths to nuget.config and csproj..."
+$currentDirectory = Get-Location
+$repoRootFolder = Split-Path -Path $currentDirectory -Parent
+$csprojFilePath = Join-Path -Path $repoRootFolder -ChildPath "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj"
+$nugetConfigFilePath = Join-Path -Path $repoRootFolder -ChildPath "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj"
 
-// pull packages
-dotnet restore "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj" --configfile "src\nuget.config"
+# pull packages
+Write-Output "Invoking dotnet restore..."
+dotnet restore $csprojFilePath --configfile $nugetConfigFilePath
 
-// x64 restore and build
-msbuild.exe "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj" /t:Restore /p:Configuration="Release" /p:RuntimeIdentifier="win-x64" /p:Platform="x64"
+# x64 restore and build
+Write-Output "Restoring 64 bit dependencies..."
+msbuild.exe $csprojFilePath /t:Restore /p:Configuration="Release" /p:RuntimeIdentifier="win-x64" /p:Platform="x64"
 
-msbuild.exe "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj" /p:Configuration="Release" /p:RuntimeIdentifier="win-x64" /p:Platform="x64" /p:UapMsixPackageBuildMode="SideloadOnly" /p:MsixPackageSigningEnabled="True" /p:PackageCertificateThumbprint=$env:LANCELOT_PFX_THUMBPRINT /p:PackageCertificatePassword=$env:LANCELOT_PFX_PASSWORD /p:GenerateAppxPackageOnBuild=true
+Write-Output "Building 64 bit MSIX package..."
+msbuild.exe $csprojFilePath /p:Configuration="Release" /p:RuntimeIdentifier="win-x64" /p:Platform="x64" /p:UapMsixPackageBuildMode="SideloadOnly" /p:MsixPackageSigningEnabled="True" /p:PackageCertificateThumbprint=$env:LANCELOT_PFX_THUMBPRINT /p:PackageCertificatePassword=$env:LANCELOT_PFX_PASSWORD /p:GenerateAppxPackageOnBuild=true
 
-// x86 restore and build
-msbuild.exe "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj" /t:Restore /p:Configuration="Release" /p:RuntimeIdentifier="win-x86" /p:Platform="x86"
+# x86 restore and build
+Write-Output "Restoring 64 bit dependencies..."
+msbuild.exe $csprojFilePath /t:Restore /p:Configuration="Release" /p:RuntimeIdentifier="win-x86" /p:Platform="x86"
 
-msbuild.exe "src\MediaFileManager\MediaFileManager.Desktop\MediaFileManager.Desktop.csproj" /p:Configuration="Release" /p:RuntimeIdentifier="win-x86" /p:Platform="x86" /p:UapMsixPackageBuildMode="SideloadOnly" /p:MsixPackageSigningEnabled="True" /p:PackageCertificateThumbprint=$env:LANCELOT_PFX_THUMBPRINT /p:PackageCertificatePassword=$env:LANCELOT_PFX_PASSWORD /p:GenerateAppxPackageOnBuild=true
+Write-Output "Building 64 bit MSIX package..."
+msbuild.exe $csprojFilePath /p:Configuration="Release" /p:RuntimeIdentifier="win-x86" /p:Platform="x86" /p:UapMsixPackageBuildMode="SideloadOnly" /p:MsixPackageSigningEnabled="True" /p:PackageCertificateThumbprint=$env:LANCELOT_PFX_THUMBPRINT /p:PackageCertificatePassword=$env:LANCELOT_PFX_PASSWORD /p:GenerateAppxPackageOnBuild=true
+
+Write-Output "Done! Check MediaFileManager.Desktop\bin\[ARCH]\Release\[TFM]\[RID]\AppPackages for msix files."
