f772147fc07882c3d38fcd1a4ac29fb849cbf740
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-11-20 21:41:19 -0500

    [feature] Added initial music file logic
    Moved some personal side project code into the starter for the music file manager view. This code will scann a folder containing mp3s, read all the metadata tags and then create artist/album/song folder structure.
    
    then copy the file into the new destinationand use the tag song name for the file name

---
+++ src/MediaFileManager/MediaFileManager.Desktop/Models/Audio/Song.cs
@@ -0,0 +1,20 @@
+﻿namespace MediaFileManager.Desktop.Models.Audio
+{
+    public class Song : BindableBase
+    {
+        private string name;
+        private string filePath;
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        public string FilePath 
+        { 
+            get => filePath; 
+            set => SetProperty(ref filePath, value);
+        }
+    }
+}
