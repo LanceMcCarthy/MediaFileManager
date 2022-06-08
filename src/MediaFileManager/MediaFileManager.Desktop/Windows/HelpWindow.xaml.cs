8a5c38ae0750b2398b953dadf46f214a9f6ee7cd
Lance McCarthy <lance@dvlup.com>
2022-06-08 14:08:44 -0400

    Added manual update check button and logic
    The button is still hidden, but will be enabled in the future.

--- src/MediaFileManager/MediaFileManager.Desktop/Windows/HelpWindow.xaml.cs
+++ src/MediaFileManager/MediaFileManager.Desktop/Windows/HelpWindow.xaml.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using MarkdownSharp;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Windows;
-using MarkdownSharp;
-using Microsoft.AppCenter.Crashes;
 using Telerik.Windows.Controls;
 using Telerik.Windows.Documents.FormatProviders.Html;
+using Windows.ApplicationModel;
+using Windows.Management.Deployment;
 
 namespace MediaFileManager.Desktop.Windows
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Irrelevant")]
     public partial class HelpWindow : RadWindow
     {
         public HelpWindow()
@@ -16,8 +19,7 @@ namespace MediaFileManager.Desktop.Windows
             InitializeComponent();
             Loaded += HelpWindow_Loaded;
         }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Irrelevant")]
+        
         private async void HelpWindow_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -41,6 +43,48 @@ namespace MediaFileManager.Desktop.Windows
             {
                 BusyIndicator.IsBusy = false;
                 BusyIndicator.Visibility = Visibility.Collapsed;
+            }
+        }
+        
+        private void RadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. check for update
+
+            // todo - Having trouble with GetAwaiter, switched to synchronous GetResults instead
+            PackageUpdateAvailabilityResult result = Package.Current.CheckUpdateAvailabilityAsync().GetResults();
+
+            switch (result.Availability)
+            {
+                case PackageUpdateAvailability.Available:
+                case PackageUpdateAvailability.Required:
+                    var popupResult = MessageBox.Show("an update is available, do you want to apply it now?");
+                    if (popupResult == MessageBoxResult.Yes)
+                        InstallUpdate();
+                    break;
+                case PackageUpdateAvailability.NoUpdates:
+                    MessageBox.Show("You are running the latest version.");
+                    break;
+                case PackageUpdateAvailability.Unknown:
+                    MessageBox.Show("It is unknown if there is an update available.");
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        private void InstallUpdate()
+        {
+            var pm = new PackageManager();
+
+            // todo - Having trouble with GetAwaiter, switched to synchronous GetResults instead
+            var updateRequestResult = pm.RequestAddPackageByAppInstallerFileAsync(
+                new Uri("https://dvlup.blob.core.windows.net/general-app-files/Installers/MediaFileManager/index.html/PackageProject.appinstaller"),
+                AddPackageByAppInstallerOptions.ForceTargetAppShutdown,
+                pm.GetDefaultPackageVolume()).GetResults();
+
+            if (updateRequestResult.ExtendedErrorCode != null)
+            {
+                var errorText = updateRequestResult.ErrorText;
             }
         }
     }
